dfdbdda812222c3d21a7637a89bdaff7d8c1c6d0
jquezada10 <quezadamotoche@hotmail.com>
2024-12-02 14:58:26 -0500

    add all-field from filter-location on backorder

--- src/components/backorder/FormFilterBackOrder.tsx
+++ src/components/backorder/FormFilterBackOrder.tsx
@@ -89,6 +89,7 @@ export default function FormFilterBackOrder() {
               onChange={(e) => { handleFilterFields('locationOrd', e.target.value); }}
               defaultValue={valueLocation}
             >
+              <FormControlLabel value="ALL" control={<Radio />} label="All" />
               <FormControlLabel value="MAIN" control={<Radio />} label="Main" />
               <FormControlLabel value="WAYNE" control={<Radio />} label="Wayne" />
             </RadioGroup>
